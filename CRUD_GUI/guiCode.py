69ce33bc13df15a1ae71bcdda281c43d3017a84c
Brandon Sandoval <colima_97@hotmail.com>
2019-07-18 16:25:00 -0500

    Added methods to fill comboBoxes in tabWidget(). Tests needed

--- CRUD_GUI/guiCode.py
+++ CRUD_GUI/guiCode.py
@@ -4,10 +4,18 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow, QApplication
 
 class Db_GUI(QMainWindow):
+   
+
     def __init__(self):
         super().__init__()
         uic.loadUi("myGUI.ui", self)
-        self.conection()
+        #Defining global variables
+        strError = 'Ocurrió un error'
+        strOK = 'Todo marcha sobre ruedas'
+        self.conection()        
+        self.fn_comboBox_fill()
+        self.tabs = self.tabWidget()
+        self.tabs.currentChanged.connect(self.fn_comboBox_fill)
 
     #Conexión
     def conection(self):
@@ -22,7 +30,32 @@ class Db_GUI(QMainWindow):
         finally:
             self.status_label.setText(message)
 
-    def fn_create(self):
+    def fn_tables(self):
+        try: 
+            cursor = db.cursor()
+            consultAllTables = 'SHOW FULL TABLES FROM mundo_peludo;'
+            cursor.execute(consultAllTables)
+            result = cursor.fetchall()
+            return result            
+        except:
+            self.status_label.setText(self.strError)
+            return "Empty" 
+
+    def fn_comboBox_fill(self):
+        tables = self.fn_tables()
+        if(tables != 'Empty'):
+            #Imprimir bonito
+            #El índice donde está el usuario en el TabWidget
+            index = self.tabs.currentIndex()
+
+            if(index == 0):
+                comboBox = self.tables_cb()
+            else:
+                comboBox = self.tables_cb1()
+
+            for row in tables:
+                table = row[0]
+                comboBox.addItem(table)                 
 
 
 

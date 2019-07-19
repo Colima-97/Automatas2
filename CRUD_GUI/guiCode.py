95e43ceafda57e75e7aa23f7eb2dade95006a2cb
Brandon Sandoval <colima_97@hotmail.com>
2019-07-18 23:50:54 -0500

    Added the function for showing information in the table

--- CRUD_GUI/guiCode.py
+++ CRUD_GUI/guiCode.py
@@ -12,9 +12,12 @@ class Db_GUI(QMainWindow):
         #Defining variables
         self.strError = 'Ocurrió un error'
         self.strOK = 'Todo marcha sobre ruedas'
-                
+        #Filling comboBox        
         self.fn_comboBox_fill()        
         self.tabWidget.currentChanged.connect(self.fn_comboBox_fill)
+        #Buttons clicked
+        self.show_button.clicked.connect(self.fn_show)
+        #self.insert_button.clicked.connect(self.fn_insert)
 
     #Conexión
     def conection(self):
@@ -40,16 +43,13 @@ class Db_GUI(QMainWindow):
             return result            
         except:
             self.status_label.setText(self.strError)
-            #bandera
-            print('error')
             return "Empty"
         finally:
             db.close() 
 
     def fn_comboBox_fill(self):
         tables = self.fn_tables()
-        if(tables != 'Empty'):
-            #Imprimir bonito
+        if(tables != 'Empty'):           
             #El índice donde está el usuario en el TabWidget
             index = self.tabWidget.currentIndex()
 
@@ -64,6 +64,49 @@ class Db_GUI(QMainWindow):
                 for row in tables:
                     table = row[0]
                     self.tables_cb1.addItem(table)                 
+
+    def fn_show(self):
+        try:
+            db = self.conection()
+            cursor = db.cursor()
+            cursor2 = db.cursor()
+
+            #El índice donde está el usuario en el TabWidget
+            index = self.tabWidget.currentIndex()
+            if (index == 0):
+                actualTable = self.tables_cb.currentText()
+            else:                
+                actualTable = self.tables_cb1.currentText()
+            #Cadena para búsqueda SQL
+            sqlColumns = 'show columns from {0}'.format(actualTable) #Conocer las columnas
+            sqlSearch = 'select * from {0};'.formart(actualTable)                            
+            cursor.execute(sqlColumns)
+            cursor2.execute(sqlSearch)            
+            resultado = cursor.fetchall()
+            resultado2 = cursor2.fetchall()
+            
+            counter = 0
+            self.table_insert.setRowCount(0)
+            for column in resultado:
+                columnName = column[0]                
+                #Poner el nombre en el label de la columna
+                self.table_insert.setHorizontalHeaderItem(counter).setText(columnName)
+                couter += 1
+            
+            for fila_num, fila_dato in enumerate(resultado2):
+                    self.table_insert.insertRow(fila_num)
+                    for col_num, dato in enumerate(fila_dato):
+                        self.table_insert.setItem(fila_num, col_num, QtWidgets.QtableWidgetItem(str(dato)))
+
+            self.status_label.setText(self.strOK)
+            cursor.close()
+            cursor2.close()
+        except:
+            self.status_label.setText(self.strError)
+        finally:
+            db.close()
+
+    #def fn_insert(self):
 
 
             
